b5f026f4246b915b12425ff11393ab5d5f45f1ef
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-03 15:47:18 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,8 @@ class Triangle
   attr_reader :side1, :side2, :side3
   attr_accessor :equilateral, :isosceles, :scalene
 
-#  def initialize(side1:, side2:, side3:)
+  def initialize(attributes)
+ # def initialize(side1:, side2:, side3:)
 #    @side1 = side1
 #    @side2 = side2
 #    @side3 = side3
