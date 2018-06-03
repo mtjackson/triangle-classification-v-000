1b795f743968b80b54080f40ca06f6bf55fdc2d9
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-03 15:47:31 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,7 +3,8 @@ class Triangle
   attr_accessor :equilateral, :isosceles, :scalene
 
   def initialize(attributes)
-    attributes.each do |key, value
+    attributes.each do |key, value|
+      self.send
  # def initialize(side1:, side2:, side3:)
 #    @side1 = side1
 #    @side2 = side2
