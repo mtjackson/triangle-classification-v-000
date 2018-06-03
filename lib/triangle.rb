0489ff2c8ed5ce517f38989d518981a5c94ff78e
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-03 15:47:55 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,6 +5,7 @@ class Triangle
   def initialize(attributes)
     attributes.each do |key, value|
       self.send(("#{key}="), value)
+    end
  # def initialize(side1:, side2:, side3:)
 #    @side1 = side1
 #    @side2 = side2
