54f047e9f0ff8a6f99286e6a428a58640ef1a7be
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-03 16:02:00 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,11 +13,8 @@ class Triangle
 
   def kind
     if @side1 + @side2 + @side3 == 0
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-        puts error.nosize
-      end
+      raise TriangleError
+
     elsif @side1 < 0 || @side2 < 0 || @side3 < 0
       raise TriangleError
     elsif @side1 + @side2 <= @side3 || @side1 + @side3 <= @side2 || @side2 + @side3 <= @side1
