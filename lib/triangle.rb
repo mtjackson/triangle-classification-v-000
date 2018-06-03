a33172e48f25adcbaf0677dfc6debb3e78d73988
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-03 15:58:45 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -17,7 +17,8 @@ class Triangle
     elsif @side1 < 0 || @side2 < 0 || @side3 < 0
       raise TriangleError
     elsif @side1 + @side2 < @side3 || @side1 + @side3 < @side2 || @side2 + @side3 < @side1
-    if @side1 == @side2 && @side2 == @side3
+      raise TriangleError
+    elsif @side1 == @side2 && @side2 == @side3
       :equilateral
     elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
       :isosceles
