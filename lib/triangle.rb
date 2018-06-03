d10fc0a60a57bccc30dc6dcf5af21f7cfd98b269
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-03 16:02:05 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,6 @@ class Triangle
   def kind
     if @side1 + @side2 + @side3 == 0
       raise TriangleError
-
     elsif @side1 < 0 || @side2 < 0 || @side3 < 0
       raise TriangleError
     elsif @side1 + @side2 <= @side3 || @side1 + @side3 <= @side2 || @side2 + @side3 <= @side1
@@ -29,11 +28,5 @@ class Triangle
   end
 
   class TriangleError < StandardError
-    def no_size
-      "Triangles must have sides greater than 0"
-    end
-    def inequality
-      "Triangles must conform to tringle equality"
-    end
   end
 end
