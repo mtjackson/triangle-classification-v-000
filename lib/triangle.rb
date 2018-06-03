811db74fd55fab2eb47711b9fdb1d8af5ec277fc
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-03 15:43:46 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,6 +13,7 @@ class Triangle
       self.kind = @equilateral
     elsif @side1 == @side2 || @side2 == @side3 || @side1 == @side3
       self.kind = @isosceles
+    elsif
 
   class TriangleError < StandardError
   end
